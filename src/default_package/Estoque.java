1dc60eaa529ed7eb66b9e9668ca0e4021fd5f715
Mateus Ramos da Silva <45329846+BoltzBit@users.noreply.github.com>
2023-01-04 22:11:41 -0300

    Add comments

--- src/default_package/Estoque.java
+++ src/default_package/Estoque.java
@@ -3,6 +3,7 @@ package default_package;
 public class Estoque {
 	Produto[] produtos;
 	
+	//colocar modificador de visibilidade public/private
 	void listarProdutos() {
 		System.out.println("Produtos em estoque:");
 		System.out.println("---------------------");
