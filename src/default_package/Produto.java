8c1b8007a67b0c9da7eb383b4d2bb64b0f92023a
Mateus Ramos da Silva <45329846+BoltzBit@users.noreply.github.com>
2023-01-04 22:09:09 -0300

    Add comments
    Pesquisar sobre visibilidade de metodos e atributos em OO

--- src/default_package/Produto.java
+++ src/default_package/Produto.java
@@ -1,10 +1,11 @@
 package default_package;
 
 public class Produto {
-	
+	//colocar modificador de visibilidade public/private
 	String descricao;
 	int quantidade;
 	
+	//colocar modificador de visibilidade public/private
 	void descrever() {
 		System.out.println(descricao + " - " + quantidade + " itens");
 	}
